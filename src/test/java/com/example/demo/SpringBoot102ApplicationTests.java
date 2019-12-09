92f14db3a1d6da01e5f11772a75c8be612c10957
tminalu <minalutem@gmail.com>
2019-12-09 14:12:00 -0500

    Variable

---
+++ src/test/java/com/example/demo/SpringBoot102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot102ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
